1a071a5a15da968944d5f55c6dfc747abe67d4e6
viniciuscasarin <casarinvinicius@gmail.com>
2024-04-19 21:09:34 -0300

    constants

---
+++ src/pages/Pets/Pets.constants.ts
@@ -0,0 +1,60 @@
+export const filterColumns = [
+  {
+    name: 'type',
+    title: 'Espécie',
+    options: [
+      {
+        value: '',
+        text: 'Todos',
+      },
+      {
+        value: 'cachorro',
+        text: 'Cachorros',
+      },
+      {
+        value: 'gato',
+        text: 'Gatos',
+      },
+    ],
+  },
+  {
+    name: 'size',
+    title: 'Porte',
+    options: [
+      {
+        value: '',
+        text: 'Todos',
+      },
+      {
+        value: 'pequeno',
+        text: 'Pequeno',
+      },
+      {
+        value: 'medio',
+        text: 'Médio',
+      },
+      {
+        value: 'grande',
+        text: 'Grande',
+      },
+    ],
+  },
+  {
+    name: 'gender',
+    title: 'Sexo',
+    options: [
+      {
+        value: '',
+        text: 'Todos',
+      },
+      {
+        value: 'femea',
+        text: 'Fêmea',
+      },
+      {
+        value: 'macho',
+        text: 'Macho',
+      },
+    ],
+  },
+]
